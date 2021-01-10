f49df961fda97f3655e7d39999788187e61266af
RithMantilla <mantilla.rith@gmail.com>
2021-01-09 23:49:41 -0500

    feat(1d)crear layout

--- Elearning/src/app/modules/contactenos/contactenos-routing.module.ts
+++ Elearning/src/app/modules/contactenos/contactenos-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactoComponent } from './contacto/contacto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ContactoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
