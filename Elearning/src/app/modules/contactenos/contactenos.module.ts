0b184079cf9a9a2134cc6b2ad1814d4cdd51da94
RithMantilla <mantilla.rith@gmail.com>
2021-01-09 22:59:58 -0500

    feat(1a): Crear una estructura escalable y modular del proyecto

---
+++ Elearning/src/app/modules/contactenos/contactenos.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ContactenosRoutingModule } from './contactenos-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ContactenosRoutingModule
+  ]
+})
+export class ContactenosModule { }
