7e53378d7668806439623c110458fb9de67edc17
RithMantilla <mantilla.rith@gmail.com>
2021-01-10 22:37:23 -0500

    feat (2a)

--- Elearning/src/app/core/core.module.ts
+++ Elearning/src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { ErrorComponent } from './error/error.component';
 
 
 @NgModule({
-  declarations: [NavbarComponent, FooterComponent],
+  declarations: [NavbarComponent, FooterComponent, ErrorComponent],
   imports: [
     CommonModule
   ],
